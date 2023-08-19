17c21b00ca9915c26cbcbfa8df366b67d9f5d8a2
dagraac <dagraac@naver.com>
2023-08-19 22:44:55 +0900

    work

---
+++ Editor/UIGraphicEditor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace DagraacSystems.Unity
+{
+	[CustomEditor(typeof(UIGraphic))]
+	public class UIGraphicEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			//base.OnInspectorGUI();
+		}
+	}
+}
